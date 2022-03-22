a4a02c040284b6b516504390b64837ec24f01449
Saikat Adhikari <saikat.adhikari@launchventures.co>
2022-03-22 23:32:42 +0530

    Rate limit error handling

--- index.ts
+++ index.ts
@@ -7,25 +7,31 @@ import { FixerService } from './src/services/FixerService/FixerService';
 import { ExchangeRateService } from './src/services/ExchangeRateService/ExchangeRateService';
 import { rateLimit } from 'express-rate-limit';
 
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
 const authService = new AuthService(
     process.env.JWT_SECRET ? process.env.JWT_SECRET : ''
 );
-
 const restCountriesService = new RestCountriesService(
     process.env.RESTCOUNTRIES_BASE_URL ? process.env.RESTCOUNTRIES_BASE_URL : ''
 );
-
 const fixerService = new FixerService(
     process.env.FIXER_BASE_URL ? process.env.FIXER_BASE_URL : '',
     process.env.FIXER_API_KEY ? process.env.FIXER_API_KEY : ''
 );
-
 const exchangeRateService = new ExchangeRateService(
     restCountriesService,
     fixerService
 );
 
-const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+const rateLimitMiddleware = rateLimit({
+    windowMs: process.env.RATE_LIMIT_WINDOW
+        ? parseInt(process.env.RATE_LIMIT_WINDOW)
+        : 60 * 1000, // 1 minutes
+    max: process.env.RATE_MAX_LIMIT ? parseInt(process.env.RATE_MAX_LIMIT) : 30, // Limit each IP to 30 requests per minute
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false,
+});
 
 const server = new Server();
 
@@ -36,35 +42,16 @@ server
     })
     .addGraphqlEndpoint(
         '/api',
-        [
-            rateLimit({
-                windowMs: 60 * 1000, // 1 minutes
-                max: 30, // Limit each IP to 30 requests per minute
-                standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-                legacyHeaders: false,
-            }),
-            authService.getAuthMiddleWare(),
-        ],
+        [rateLimitMiddleware, authService.getAuthMiddleWare()],
         {
             schema: getCurrencyExchangeSchema(exchangeRateService),
             graphiql: true,
         }
     )
-    .addGraphqlEndpoint(
-        '/api-open',
-        [
-            rateLimit({
-                windowMs: 60 * 1000, // 1 minutes
-                max: 30, // Limit each IP to 30 requests per minute
-                standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-                legacyHeaders: false,
-            }),
-        ],
-        {
-            schema: getCurrencyExchangeSchema(exchangeRateService),
-            graphiql: true,
-        }
-    );
+    .addGraphqlEndpoint('/api-open', [rateLimitMiddleware], {
+        schema: getCurrencyExchangeSchema(exchangeRateService),
+        graphiql: true,
+    });
 
 server.start(port, () => {
     console.log(`Server started at ${port}`);
--- src/schema/currencyExchange/resolver.ts
+++ src/schema/currencyExchange/resolver.ts
@@ -7,16 +7,24 @@ export const getCurrencyExchangeResolver = (
     Query: {
         countryDetails: async (
             _: any,
-            { name, targetCurrency }: { name: string; targetCurrency: string }
+            { name, targetCurrency }: { name: string; targetCurrency: string },
+            info: any
         ) => {
+            if (info.rateLimit && info.rateLimit.remaining === 0) {
+                throw new Error(
+                    `Request limit exceeded, retry at ${info.rateLimit.resetTime}`
+                );
+            }
             const exchangeRateResponse =
                 await exchangeRateService.getExchangeRatesByCountryName(
                     name,
                     targetCurrency
                 );
+
             if (isLeft(exchangeRateResponse)) {
                 return exchangeRateResponse.left;
             }
+
             throw new Error(exchangeRateResponse.right);
         },
     },
